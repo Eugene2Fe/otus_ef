74838a622cb43467bedea78a17dea555987f764c
Evgeniy Fedorushkin <e.fedorushkin@yadro.com>
2024-10-15 01:07:27 +0300

    +task3 : added print top uniq gamer - scores

--- high_scores.cpp
+++ high_scores.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <sstream>
 #include "high_scores.h"
+#include <algorithm>
+#include <map>
 
 
 std::string ask_name(std::string user_name) {
@@ -26,8 +30,7 @@ void save_top_results(std::string user_name, int attempts_count, std::string hig
 		// out_file.close();
 }
 
-void print_top_results(std::string high_scores_filename)
-	{
+void print_all_results(std::string high_scores_filename){
 		std::ifstream in_file{high_scores_filename};
 		// in_file.open(high_scores_filename); // окрываем файл для чтения
 		if (!in_file.is_open()) {
@@ -56,3 +59,51 @@ void print_top_results(std::string high_scores_filename)
 		}
 		std::cout << std::endl;
 	}
+
+
+struct Gamer_Ochki {
+    std::string gamer;
+    int ochkov;
+};
+
+bool compare_scores(const Gamer_Ochki &a, const Gamer_Ochki &b) {
+    return a.ochkov < b.ochkov;
+}
+
+void print_top_results(std::string high_scores_filename) {
+    std::cout << "Highest scores:" << std::endl;
+    std::ifstream inputFile(high_scores_filename);
+
+    if (inputFile.is_open()) {
+        std::map<std::string, int> unique_games_results;
+        std::string line;
+
+        // Чтение и обновление результатов в map
+        while (std::getline(inputFile, line)) {
+            std::istringstream iss(line);
+            Gamer_Ochki gamer_i_ochki;
+            iss >> gamer_i_ochki.gamer >> gamer_i_ochki.ochkov;
+
+            if (unique_games_results.find(gamer_i_ochki.gamer) == unique_games_results.end() ||
+                unique_games_results[gamer_i_ochki.gamer] > gamer_i_ochki.ochkov) {
+                   unique_games_results[gamer_i_ochki.gamer] = gamer_i_ochki.ochkov;
+            }
+        }
+
+        inputFile.close();
+
+        std::vector<Gamer_Ochki> all_games_results;
+        for (const auto &entry : unique_games_results) {
+            all_games_results.push_back({entry.first, entry.second});
+        }
+
+        std::sort(all_games_results.begin(), all_games_results.end(), compare_scores);
+
+        for (const auto &one_gamer : all_games_results) {
+            std::cout << one_gamer.gamer << " " << one_gamer.ochkov << std::endl;
+        }
+
+    } else {
+        std::cout << "\nFailed to open file: " << high_scores_filename << std::endl;
+    }
+}
--- high_scores.h
+++ high_scores.h
@@ -6,4 +6,5 @@
 std::string ask_name(std::string user_name);
 void save_top_results(std::string gamer, int count, std::string high_scores_filename);
 void print_top_results(std::string high_scores_filename);
+void print_all_results(std::string high_scores_filename);
 
