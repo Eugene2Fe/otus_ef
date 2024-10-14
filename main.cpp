b04788337241b47f4d550000625db3768db926ad
Evgeniy Fedorushkin <e.fedorushkin@yadro.com>
2024-10-14 20:58:11 +0300

    +task1 : added --max arg

--- main.cpp
+++ main.cpp
@@ -3,14 +3,32 @@
 #include "random_value.h"
 #include "high_scores.h"
 
-int main() {
+int main(int argc, char** argv) {
+
+    int max_random_val = 10;
+
+    std::string arg1_value{argv[1]};
+    if (arg1_value == "--max") {
+        int max_random_val = 0;
+        if (argc < 3) {
+            std::cout << "Wrong usage! The argument '-max' requires some value!" << std::endl;
+            return -1;
+        }
+        max_random_val = std::atoi(argv[2]);
+        std::cout << "Max value bay be before 0 and " << argv[2] << std::endl;
+    }
+    else
+    {
+        std::cout << "Max value bay be before 0 and " << max_random_val << std::endl;
+    }
+
 
     bool win = false;
     std::string gamer_name;
-	// const int target_value = 5;
-	const int target_value = gen_random_value(10);
+	const int target_value = gen_random_value(max_random_val);
 	int current_value = 0;
     int attempts_count = 1;
+
     const std::string high_scores_filename = "../high_scores.txt";
 
     print_top_results(high_scores_filename);
@@ -20,9 +38,9 @@ int main() {
     while (!(win))
     {
 
-	std::cout << "Enter your number:" << std::endl;
+	std::cout << "Enter your number: " << std::endl;
     std::cin >> current_value;
-	std::cout << "INFO: Your number is:" << current_value << std::endl;
+	std::cout << "INFO: Your number is: " << current_value << std::endl;
 
 	if (current_value > target_value) {
 		std::cout << "less than " << current_value << std::endl;
@@ -33,9 +51,9 @@ int main() {
         attempts_count++;
 	}
 	else {
-		std::cout << "you win!" << std::endl;
+		std::cout  << gamer_name << ", you win! "<< std::endl;
         win = true;
-        std::cout << "Count of attemps:" << attempts_count << std::endl;
+        std::cout << "Count of attemps: " << attempts_count << std::endl;
 		break;
 	}
 
