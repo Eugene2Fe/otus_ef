3435eceb7c1cc692cf6d5ce212e15b427379d349
Evgeniy Fedorushkin <e.fedorushkin@yadro.com>
2024-11-12 01:47:19 +0300

    remove debug constructor print

--- Velocity.hpp
+++ Velocity.hpp
@@ -8,18 +8,15 @@ class Velocity {
         Point vec;
     public:
         inline Velocity() {
-            std::cout << "Default constructor called" << std::endl;
         }
 
         inline Velocity(double abs, double angle) {
-            std::cout << "Constructor with abs and angle called" << std::endl;
             const double x = std::cos(angle);
             const double y = std::sin(angle);
             vec = Point{x, y} * abs;
         }
 
         inline Velocity(const Point& vector) {
-            std::cout << "Constructor with Point vector called" << std::endl;
             setVector(vector);
         }
 
