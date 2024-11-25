1635da1f9eb3d91136398efba7b25d8137f4c286
Evgeniy Fedorushkin <e.fedorushkin@yadro.com>
2024-11-26 01:53:49 +0300

    done list-container and test fot him

---
+++ 06_hw_containers/list_container.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <stdexcept> // lib for exception print
+
+class ListContainer {
+private:
+    struct Node {
+        int value;    // Значение элемента узла
+        Node* prev;   // Указатель на предыдущий элем
+        Node* next;   // Указатель на следующий элем
+
+        // Конструктор структуры элемента узла
+        Node(int val) : value(val), prev(nullptr), next(nullptr) {}
+    };
+
+    Node* head;       // Указатель на начало списка
+    Node* tail;       // Указатель на конец списка
+    size_t counter;     // Количество элементов в списке
+
+public:
+    // default Constr
+    ListContainer() : head(nullptr), tail(nullptr), counter(0) {}
+
+    // Destr for make free memory
+    ~ListContainer() {
+        while (head) {
+            Node* temp = head; // сейвим текущий узел
+            head = head->next; // Переходим к следующему узлу
+            delete temp; // и удаляем текущий
+        }
+    }
+
+    // 1. добавляет элемент в конец списка
+    void push_back(int value) {
+        Node* newNode = new Node(value); // Создаём новый узел
+        if (!tail) { // Если список пуст
+            head = tail = newNode;
+        } else { // Если есть элементы
+            tail->next = newNode; // Привязываем новый узел к концу
+            newNode->prev = tail;
+            tail = newNode; // Обновляем указатель на конец
+        }
+        ++counter; // Увеличиваем счётчик
+    }
+
+    // 2. вставка элемента на точную указанную позицию
+    void insert(size_t index, int value) {
+        if (index > counter) {
+            throw std::out_of_range("position out of range");
+        }
+
+        Node* newNode = new Node(value); // Создание нового узла
+
+        if (index == 0) {
+            // в начало
+            newNode->next = head;
+            if (head) head->prev = newNode;
+            head = newNode;
+            if (!tail) tail = head;
+        } else if (index == counter) {
+            // в конец
+            push_back(value);
+            return;
+        } else {
+            // по индексу позиции
+            Node* current = head;
+            for (size_t i = 0; i < index; ++i) {
+                current = current->next;
+            }
+            newNode->next = current;
+            newNode->prev = current->prev;
+            current->prev->next = newNode;
+            current->prev = newNode;
+        }
+        ++counter;
+    }
+
+    // 3. удаление элемента по точной указанной позиции
+    void erase(size_t index) {
+        if (index >= counter) {
+            throw std::out_of_range("position out of range");
+        }
+
+        Node* current = head;
+        for (size_t i = 0; i < index; ++i) {
+            current = current->next;
+        }
+
+        if (current->prev) { // Убираем связь с предыдущим узлом
+            current->prev->next = current->next;
+        } else { // На случай если удаляем первый узел
+            head = current->next;
+        }
+
+        if (current->next) { // Убираем связь с следующим узлом
+            current->next->prev = current->prev;
+        } else { // На случай если удаляем последний узел
+            tail = current->prev;
+        }
+
+        delete current;
+        --counter;
+    }
+
+    // 4. получение размера контейнера
+    size_t size() const {
+        return counter;
+    }
+
+    void print() const {
+        Node* current = head;
+        while (current) { // проходим по всем узлам и печать их
+            std::cout << current->value << " ";
+            current = current->next;
+        }
+        std::cout << std::endl;
+    }
+};
--- 06_hw_containers/main.cpp
+++ 06_hw_containers/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "seq_container.cpp"
+#include "list_container.cpp"
 
 int main() {
     std::cout << "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- \n";
@@ -34,15 +35,35 @@ int main() {
     std::cout << "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- \n \n";
 
 
-    // std::cout << "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- \n";
-    // std::cout << "     Списковый контейнер    \n";
-    // SeqContainer seqContainer;
+    std::cout << "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- \n";
+    std::cout << "     Списковый контейнер    \n";
+    ListContainer listContainer;
 
-    // for (int i = 0; i < 10; ++i) {
-    //     seqContainer.push_back(i);
-    // }
-    // std::cout << " Добавляем 10 элементов:\n";
-    // seqContainer.print();
+    for (int i = 0; i < 10; ++i) {
+        listContainer.push_back(i);
+    }
+    std::cout << " Добавляем 10 элементов:\n";
+    listContainer.print();
+
+    std::cout << "Размер контейнера: " << listContainer.size() << " элементов. "<< std::endl;
+    std::cout << "Удаляем элементы 3, 5, 7 и выводим что получилось: \n";
+    listContainer.erase(2);
+    listContainer.erase(3);
+    listContainer.erase(4);
+    listContainer.print();
+
+    std::cout << "Добавляем число 10 в нулевую позицию и печатаем: \n";
+    listContainer.insert(0, 10);
+    listContainer.print();
+
+    std::cout << "Добавляем число 20 на 4-ую)(5ю) позицию и печатаем: \n";
+    listContainer.insert(4, 20);
+    listContainer.print();
+
+    std::cout << "Добавляем число 30 в конец и печатаем: \n";
+    listContainer.push_back(30);
+    listContainer.print();
+
 
 }
 
